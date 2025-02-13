4a0a6d6730f05fab2b3b7bb54dacbc7bd570092e
lilachbitton <lilacbra@gmail.com>
2025-02-13 23:50:38 +0200

    return

--- src/app/admin/cycles/[cycleId]/lessons/[lessonId]/assignments/page.js
+++ src/app/admin/cycles/[cycleId]/lessons/[lessonId]/assignments/page.js
@@ -293,7 +293,7 @@ const updateAssignmentStatus = async (assignmentId, newStatus) => {
     return <div className="p-4 text-center">לא נמצא שיעור</div>;
   }
 
-  returnreturn (
+  return  (
     <div className="p-4" dir="rtl">
       {showSuccessAlert && (
         <div className="fixed top-4 right-4 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded z-50">
